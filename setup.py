d26a5ed0de58a4c4f8035898dcdab744055c3e70
Kevin <kbhowbrook@gmail.com>
2022-07-14 10:06:21 +0100

    Update for release supporting wagtail 3.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 
 setup(name='wagtail-quick-create',
-      version='1.0.7',
+      version='2.0.0',
       description='Offer links to the admin user to create content under sections quickly.',
       long_description=long_description,
       long_description_content_type='text/markdown',
